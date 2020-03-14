de2e751564c90a36c650442fc471ac4193c158a8
George Bockari <bitwhys@gmail.com>
2020-03-14 16:02:39 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Vue-Playground Boilerplate
+
+## Features
+
+- Tailwindcss
+- Autoprefixer
+- Fontastic Font Icons
+  - Heroicons By Steve Schoger. (see icons-reference in public)
+  - Social Icons
+    - `.icon-twitter-alt`
+    - `.icon-youtube-atl`
+    - `.icon-github-alt`
+    - `.icon-reddit`
+    - `.icon-whatsapp`
+    - `.icon-mobileme`
+    - `.icon-sharethis`
+    - `.icon-facebook`
+- Prettier
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
+
+### Lints and fixes files
+
+```
+yarn lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <main class="">
+      <h1 class="text-6xl center">Vue Playground Boilerplate</h1>
+      <p>
+        For a guide and recipes on how to configure / customize this project,<br />
+        check out the
+        <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+          >vue-cli documentation</a
+        >.
+      </p>
+      <div>
+        <button
+          class="icon icon-arrow-up focus:outline-none
+          text-xl bg-teal-300 hover:bg-teal-400
+          text-teal-800 font-bold py-2 px-4 rounded
+          inline-flex items-center transition duration-200"
+        >
+          <span class="ml-2">Click Me</span>
+        </button>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  @apply min-h-screen bg-teal-100 grid justify-center items-center;
+}
+</style>
